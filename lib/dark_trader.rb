0ddb9c3cc5ede74628ccb10f44efde0b7758e02c
ho757uff <robindebessel.pro@gmail.com>
2023-10-12 21:02:31 +0200

    VIP - Working .rb + _spec.rb

--- lib/dark_trader.rb
+++ lib/dark_trader.rb
@@ -1,74 +1,43 @@
-require gem 'rspec'
-require gem 'pry'
-require gem 'rubocop'
-require gem 'dotenv'
-require gem 'http'
-require gem 'json'
-require gem 'nokogiri'
-require gem 'open-uri'
+require 'rspec'
+require 'pry'
+require 'rubocop'
+require 'dotenv'
+require 'http'
+require 'json'
+require 'nokogiri'
+require 'open-uri'
 
-puts "Voici quelles bloque-chiennes tu pourrais acheter avec tes faibles économies:"
+# Dotenv.load('.env')
 
-page = Nokogiri::HTML(URI.open("https://coinmarketcap.com/all/views/all/"))
-crypto_name = page.xpath('//*[@id="__next"]/div[2]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr[1]/td[2]/div/a[2]')
-
-puts crypto_name
+# ---
 
 
 
 
+puts "Voici quelles 'bloque-chiennes' tu pourrais acheter avec tes faibles économies:\n\n\n"
 
-
-
-
-# # From the website, get an array of the currencies name and convert it to string
-# currency_name_array = page.xpath("//tr/td/a[contains(@class, 'currency-name-container')]/text()").map {|x| x.to_s }
-# # From the website, get an array of the currencies price and convert it to string
-# currency_value_array = page.xpath("//tr/td/a[contains(@class, 'price')]/text()").map {|x| x.to_s }
-
-# # Convert the 2 arrays into hash
-# currency_result = Hash[currency_name_array.zip(currency_value_array)]
-
-# puts currency_result
-
-
-
-
-
-
-
-
-
-
-
-
-def get_crypto_name()
-  open  
-end
-
-def get_crypto_value()
-  
-end
-
-def get_crypto_hash()
-    
-end
-
-def get_crypto_array()
-
+def crypto_symbol_array(page)
+  crypto_symbol_elements = page.xpath('//*[@id="__next"]/div[2]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr/td[3]/div')
+  crypto_symbol_elements.map(&:text) #crypto_symbol_array
 end
 
 
+def crypto_price_array(page)
+  crypto_price_elements = page.xpath('//*[@id="__next"]/div[2]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr/td[5]/div/a/span')
+  crypto_price_elements.map(&:text) #crypto_price_array
+end
 
-def execute
-  
+def final_array(crypto_symbol_array, crypto_price_array)
+  crypto_symbol_array.zip(crypto_price_array).map { |symbol, price| {symbol => price} } #final_array
 end
 
 
-# execute
+def execute()
+  page = Nokogiri::HTML(URI.open("https://coinmarketcap.com/all/views/all/"))
+  return final_array(crypto_symbol_array(page), crypto_price_array(page))
+end
 
-# begin
-#   do_something() #partie de code qui risque de produire une erreur
-# rescue => e
-#   puts "Oups petite erreur, mais c'est pas grave" #résultat que tu veux voir en lieu et place d'une erreur terminal
-# end
+execute
+
+final_puts = execute
+puts final_puts
--- spec/dark_trader_spec.rb
+++ spec/dark_trader_spec.rb
@@ -1,9 +1,7 @@
 require_relative '../lib/dark_trader.rb'
 
-describe "the crypto scrapping method" do
-  it "scraps a webpage and gathers cryptos types and their prices" do
-
-
-    expect(greet("Alice")).to eq("Hello, Alice!")
+describe "the execute method" do
+  it "calls 2 methods to scrap a webpage and gather cryptos symbols and their corresponding values" do
+    expect(execute).not_to be_nil
   end
 end
